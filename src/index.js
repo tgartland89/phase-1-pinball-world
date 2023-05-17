3a7dec1f058173db4c420772a08ab6286a57b3c0
tgartland89 <thomas.gartland@gmail.com>
2023-05-16 23:29:27 -0600

    second deliverable

--- src/index.js
+++ src/index.js
@@ -1,27 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("http://localhost:3000/games")
-      .then(r => r.json())
-      .then(data => {
-        let gameListNav = document.querySelector(".game-list");
-        let firstGame = data[0];
+  fetch("http://localhost:3000/games")
+    .then(r => r.json())
+    .then(data => {
+      let gameListNav = document.querySelector(".game-list");
+      let gameDetailsSection = document.querySelector(".game-details");
+      let gameItems = [];
 
-        let detailImage = document.getElementById("detail-image")
-        let detailTitle = document.getElementById("detail-title")
-        let detailHighScore = document.getElementById("detail-high-score")
+      // Display game details for the selected game
+      function displayGameDetails(game) {
+        let detailImage = document.getElementById("detail-image");
+        let detailTitle = document.getElementById("detail-title");
+        let detailHighScore = document.getElementById("detail-high-score");
 
-        detailImage.src = firstGame.image
-        detailTitle.textContent = firstGame.name
-        detailHighScore.textContent = firstGame.high_score
-        
-        data.forEach(game => {
-          let gameName = game.name;
-          let gameManufacturer = game.manufacturer_name;
-          let gameItem = document.createElement("h5");
-          gameItem.textContent = `${gameName} (${gameManufacturer})`;
-          gameListNav.appendChild(gameItem);
+        detailImage.src = game.image;
+        detailTitle.textContent = game.name;
+        detailHighScore.textContent = game.high_score;
+      }
+
+      // Display all game list items and add click event listener
+      data.forEach(game => {
+        let gameName = game.name;
+        let gameManufacturer = game.manufacturer_name;
+        let gameItem = document.createElement("h5");
+        gameItem.textContent = `${gameName} (${gameManufacturer})`;
+        gameListNav.appendChild(gameItem);
+        gameItems.push(gameItem);
+
+        // Add click event listener to each game list item
+        gameItem.addEventListener("click", () => {
+          gameItems.forEach(item => {
+            item.classList.remove("active");
+          });
+          gameItem.classList.add("active");
+          displayGameDetails(game);
         });
-      })
-      .catch(error => {
-        console.error("Error fetching games:", error);
       });
-  });
+
+      // Initialize the first game details display
+      if (data.length > 0) {
+        gameItems[0].classList.add("active");
+        displayGameDetails(data[0]);
+      }
+    })
+    .catch(error => {
+      console.error("Error fetching games:", error);
+    });
+});
